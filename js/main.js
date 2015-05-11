c82db311c5ef874aa0adb03ff336fdf19ddebdb1
Kerr Marin Miller <kerr@kerrmarin.com>
2015-05-11 22:32:13 +0100

    Add bootstrap and amend main function to return bootstrap panels

--- js/main.js
+++ js/main.js
@@ -4,23 +4,54 @@ function triggerRefresh(frequency) {
     setTimeout(function() {
 		AST.manager.services.forEach(function(service) {
 		    console.log(service);
-			service.request(function(success, error) {
+			service.request(function(success, data, error) {
 				
 			});
 		});
 	}, frequency);
 }
 
-function createServiceContainers() {
+function createServiceContainers(success, data) {
 	var container = document.getElementById("content");
 	AST.manager.services.forEach(function(service) {
 	    console.log(service.name);
+		//Create the main div
 		var div = document.createElement("div");
-		div.className = "service";
+		div.className = "col-sm-4";
 		div.id = service.id;
-		var text = document.createTextNode(service.name);
-		text.className = "service-title";
-		div.appendChild(text);
+		
+		//Create the panel 
+		var panel = document.createElement("div");
+		panel.classList.add("panel");
+		panel.classList.add("panel-"+ success ? "success" : "danger");
+		
+		//Create the heading
+		var heading = document.createElement("div");
+		heading.classList.add("panel-heading");
+		//Add the title
+		var title = document.createTextNode(service.name);
+		title.className = "panel-title";
+		heading.appendChild(title);
+		
+		//Create the body
+		var body = document.createElement("div");
+		body.classList.add("panel-body");
+		//Add all the elements 
+		var list = document.createElement('ul');
+		for (var key in data) {
+		   if (data.hasOwnProperty(key)) {
+	   			var item = document.createElement('li');
+				var text = document.createTextNode(key + ": " + data[key]);
+		        item.appendChild(text);
+		        list.appendChild(item);
+		   }
+		}
+		body.appendChild(list);
+		
+		//Create the structure
+		panel.appendChild(heading);
+		panel.appendChild(body);
+		div.appendChild(panel);
 		container.appendChild(div);
 	});
 }
